a2c98784358ddba7f4b0ec136ab5ea0e05391381
Anahita Keshvari <anahita.keshvari@gmail.com>
2023-07-03 20:36:03 +0200

    Add files via upload

---
+++ lib/config.py
@@ -0,0 +1,21 @@
+
+# Wireless network
+WIFI_SSID =  "Your_WiFi_SSID"
+WIFI_PASS = "Your_WiFi_Password" 
+
+# MQTT configuration
+SERVER = "Your_Server_IP_Address"
+PORT = 1883
+USER = "Mosquitto_User"
+KEY = "Mosquitto_Password"
+CLIENT_ID = 'id-1234'  # Can be anything started with id-
+DS_TEMP_FEED = "devices/dstemperature"
+DHT_TEMP_FEED = "devices/dhttemperature"
+DHT_HUMI_FEED = "devices/dhthumidity"
+LDR_FEED = "devices/ldr"
+SOIL_FEED = "devices/soil"
+LIGHT_FEED = "devices/light"
+
+
+# WDT time
+WDT_TIMEOUT = 8000
